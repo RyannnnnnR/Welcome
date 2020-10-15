d2713b04d4816e749d5ca3aa95645cf2bd3e7104
Ryan Reichenberg <ryan.reichenberg@gmail.com>
2020-10-15 12:32:20 +1100

    Fix create script

--- create.sql
+++ create.sql
@@ -7,14 +7,14 @@ use Assignmentdb;
 go
 
 Create TABLE Orders(
-firstname varchar(50) not null primary key,
-lastname varchar(50) not null,
-noOftyres int not null,
-amount int not null);
+`firstname` varchar(50) not null primary key,
+`lastname` varchar(50) not null,
+`noOftyres` int not null,
+`amount` int not null);
 
 go
 
-INSERT INTO Orders(firstname, lastname,noOfTyres)
+INSERT INTO Orders(firstname, lastname,noOfTyres, amount)
 values('john','smith',2, 220);
 
 go
