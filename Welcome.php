499581c7e2aca3b0697a0413e39d9d248e1a1bbe
Ryan Reichenberg <ryan.reichenberg@gmail.com>
2020-10-15 12:53:39 +1100

    Fix php

--- Welcome.php
+++ Welcome.php
@@ -5,9 +5,34 @@
 </head>
 <body>
     <?php
+    $servername="127.0.0.1";
+    $user="root";
+    $password="";
+    $dbname="Assignmentdb";
 
-    echo $_GET["firstname"]." ".$_GET["lastname"].'<br/>';
-    echo "total amount due is: ".($$_GET["tyres"]* 110).'<br/>';
+    // Create connection
+    $conn = mysqli_connect($servername,$user, $password,$dbname) or die(mysqli_connect_error());
+
+    $unsafe_firstname = $_POST['firstname'];
+    $unsafe_lasstname = $_POST['lastname'];
+    $unsafe_nooftyres = $_POST['tyres'];
+    $amount= $unsafe_nooftyres*110;
+
+    $stmt = $conn->prepare("INSERT INTO Orders (firstname, lastname, noOftyres, amount)
+    VALUES (?, ?, ?,?)");
+
+    // TODO check that $stmt creation succeeded
+
+    // "s" means the database expects a string "i" means integer
+    $stmt->bind_param("ssii", $unsafe_firstname,$unsafe_lasstname,$unsafe_nooftyres,$amount);
+
+    $stmt->execute();
+
+    $stmt->close();
+
+    $conn->close();
+    echo $$_POST["firstname"]." ".$_POST["lastname"].'<br/>';
+    echo "total amount due is: ".($_POST["tyres"]* 110).'<br/>';
 
     ?>
 </body>
--- index.php
+++ index.php
@@ -5,49 +5,11 @@
 </head>
 
 <body>
-<form  action="index.php" method="post">
+<form  action="Welcome.php" method="post">
 FisrtName: <input type="text" name="firstname" value="<?php echo $fname;?>"><br/><br/>
 LastName:  <input type="text" name="lastname" value="<?php echo $lname;?>"><br/><br/>
 Number of Tyres: <input type="number" name="tyres" value="<?php echo $tyre;?>"><br/><br/>
 <input type="submit" name="Calculate"><br/>
-    
-    <?php
-    $servername="localhost";
-    $user="root";
-    $password="alpine";
-    $dbname="Assignmentdb";
-
-    // Create connection
-    $conn = new mysqli($servername,$user, $password,$dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-
-    $unsafe_firstname = $_POST['firstname'];
-    $unsafe_lasstname = $_POST['lastname'];
-    $unsafe_nooftyres = $_POST['tyres'];
-    $amount=$unsafe_nooftyres*110;
-
-    $stmt = $mysqli->prepare("INSERT INTO Orders (firstname, lastname, noOftyres, Amount)
-    VALUES (?, ?, ?,?)");
-
-    // TODO check that $stmt creation succeeded
-
-    // "s" means the database expects a string "i" means integer
-    $stmt->bind_param("ssii", $unsafe_firstname,$unsafe_lasstname,$unsafe_nooftyres,$amount);
-
-    $stmt->execute();
-
-    $stmt->close();
-
-    $mysqli->close();
-
-    $conn->close();
-    header(sprintf('Location: Welcome.php?firstname=%s&lastname=%s&tyres=%d', $unsafe_firstname, $unsafe_lasstname, $unsafe_nooftyres))
-    ?>
-    </form>
+</form>
 </body>
 </html>
